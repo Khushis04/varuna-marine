0740b1777f35c2d66a78381ff825e4d8139ba3ca
Khushis04 <127972471+Khushis04@users.noreply.github.com>
2025-11-09 16:49:48 +0530

    Create RoutesPort.ts

---
+++ backend/src/core/ports/RoutesPort.ts
@@ -0,0 +1,8 @@
+import { Route } from '@domain/Route'
+
+
+export interface RoutesPort {
+  list(): Promise<Route[]>
+  setBaseline(id: number): Promise<void>
+  getBaseline(): Promise<Route | null>
+}
