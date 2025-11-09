4b9691048c94c6a54e534a3b0ff8d1616493cafe
Khushis04 <127972471+Khushis04@users.noreply.github.com>
2025-11-09 16:51:40 +0530

    Fix formatting in PoolingPort.ts

---
+++ backend/src/core/ports/PoolingPort.ts
@@ -0,0 +1,5 @@
+import { PoolMember } from '@domain/Pooling'
+
+export interface PoolingPort {
+  createPool(year: number, members: PoolMember[]): Promise<{ members: { shipId: string; cb_after: number }[]; sum: number }>
+}
